eba0c517871ef20236d775b7d0d2e4a1ff7d31e7
Gokari-123 <mahaboobgokari@gmail.com>
2025-04-08 11:57:09 +0530

    Add files via upload

---
+++ jenkins/src/jenkins/Main.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		  System.out.println("Hello and welcome!");
+	        System.out.println("This is a test for the new branch.");
+	        System.out.println(new Date());
+	}
+}
